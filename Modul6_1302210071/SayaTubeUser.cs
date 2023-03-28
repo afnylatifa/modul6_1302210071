c49a79aab685c13ada9412efc7a80b787497d2b9
afnylatifa <127191976+afnylatifa@users.noreply.github.com>
2023-03-28 17:11:25 +0700

    penambahan design by contract pada semua kelas

--- Modul6_1302210071/SayaTubeUser.cs
+++ Modul6_1302210071/SayaTubeUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,8 @@ namespace Modul6_1302210071
 
         public SayaTubeUser(string username)
         {
+            Debug.Assert(username != null);
+            Debug.Assert(username.Length <= 100);
             this.id = id;
             this.uploadedVideos= new List<SayaTubeVideo>();
             this.username = username;
@@ -31,17 +34,17 @@ namespace Modul6_1302210071
 
         public void AddVideo(SayaTubeVideo vids)
         {
+            Debug.Assert(!uploadedVideos.Contains(vids));
+            Debug.Assert(uploadedVideos.Count >= 0 && uploadedVideos.Count < int.MaxValue);
             uploadedVideos.Add(vids);
         }
 
         public void PrintAllPlayCount()
         {
             Console.WriteLine("USer : " + username);
-            int i = 0;
-            foreach(SayaTubeVideo sayaTubeVideoin in uploadedVideos)
+            for(int i = 0; i < uploadedVideos.Count && i < 8; i++)
             {
-                i++;
-                Console.WriteLine("Video " + i + " judul " + sayaTubeVideoin.GetTitle());
+                Console.WriteLine("Video " + (i+1) + " judul " + uploadedVideos[i].GetTitle());
             }
         }
     }
